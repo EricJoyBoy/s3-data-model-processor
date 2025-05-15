fa71c72f2e340e155f5e95c7dbb2c711c4aff8eb
Eric <ericonelopes@gmail.com>
2025-05-15 22:22:39 +0200

    fix: delete duplicate class Config 2.java

--- src/main/java/org/processor/Config 2.java
+++
@@ -1,8 +0,0 @@
-package org.processor;
-
-public class Config {
-    public static int getChunkSize() {
-        try { return Integer.parseInt(System.getenv("CHUNK_SIZE")); }
-        catch (Exception e) { throw new RuntimeException("Invalid CHUNK_SIZE", e); }
-    }
-}
